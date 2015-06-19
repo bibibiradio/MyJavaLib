43b00e723fbe7d28558ade5267e921c514cbeab9
bibibiradio <qbjxiaolei@163.com>
2015-06-19 23:21:46 +0800

    delete

--- com/bibibiradio/httpsender/ResponseData.java
+++
@@ -1,9 +0,0 @@
-package com.bibibiradio.httpsender;
-
-import java.util.Map;
-
-public interface ResponseData {
-	public int getStatusCode();
-	public byte[] getResponseContent();
-	public Map<String,String> getResponseHeader();
-}
